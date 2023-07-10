29596cf4bd65b1fbfd8f977e2549505248ac224c
GERALD D NEALE <53587595+geraldneale@users.noreply.github.com>
2023-07-09 20:53:46 -0400

    Update driver.py

--- driver.py
+++ driver.py
@@ -38,6 +38,7 @@ min_fee = 10        #sometimes there is fee pressure on testnet10
 #switch ADD_DATA for environment
 #ADD_DATA = bytes.fromhex("ccd5bb71183532bff220ba46c268991a3ff07eb358e8255a65c30a2dce0e5fbb") #genesis challenge(works for mainnet)
 ADD_DATA = bytes.fromhex("ae83525ba8d1dd3f09b277de18ca3e43fc0af20d20c4b3e92ef2a48bd291ccb2")  #genesis challenge(works for testnet10)
+#ADD_DATA = bytes.fromhex("eb8c4d20b322be8d9fddbf9412016bdffe9a2901d7edb0e364e94266d0e095f7")  #genesis challenge(works for simulator0)
 
 def print_json(dict):
     print(json.dumps(dict, sort_keys=True, indent=4))
